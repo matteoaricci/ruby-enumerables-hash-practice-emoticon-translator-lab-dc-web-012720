9219d89540533b2e76909576ba3cc3d9c5b3ef3f
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-09 19:24:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
   get_new = {"get_emoticon": {}, "get_meaning": {}}
   
-puts emoticons
+pp emoticons
 end
 
 def get_japanese_emoticon
